2819bf3551d143d0cb5590520fdc5b0fb7aa1080
khumo67kid67 <kmogotsi16@outlook.com>
2026-06-07 12:34:36 +0200

    Initial files for MakeCode project

---
+++ images.g.ts
@@ -0,0 +1,172 @@
+// Auto-generated code. Do not edit.
+namespace myImages {
+
+    helpers._registerFactory("image", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("animation", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "MC":
+            case "anim1":return [img`
+. . . . 5 5 5 5 5 5 5 . . . . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+. 5 5 5 f 5 5 5 5 5 5 5 5 5 . . 
+. 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+f f f f f f f f f 5 5 5 5 5 5 . 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 . 
+. 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+. . . . 5 5 5 5 5 5 5 . . . . . 
+. . . . . . . . . . . . . . . . 
+`, img`
+. . . . 5 5 5 5 5 5 5 . . . . . 
+. . 5 5 5 5 5 5 5 5 5 5 5 . . . 
+. 5 5 5 f 5 5 5 5 5 5 5 5 5 . . 
+. 5 5 5 5 5 5 5 5 5 5 5 5 5 . . 
+. . . 5 5 5 5 5 5 5 5 5 5 5 5 . 
+. . . . . . 5 5 5 5 5 5 5 5 5 . 
+. . . . . . . 5 5 5 5 5 5 5 5 . 
+. . . . . . . . 5 5 5 5 5 5 5 . 
+. . . . . . . 5 5 5 5 5 5 5 5 . 
+. . . . . . 5 5 5 5 5 5 5 5 5 . 
+. . . . . 5 5 5 5 5 5 5 5 5 5 . 
+. . . . . 5 5 5 5 5 5 5 5 5 . . 
+. . . . 5 5 5 5 5 5 5 5 5 5 . . 
+. . . 5 5 5 5 5 5 5 5 5 5 . . . 
+. . . . 5 5 5 5 5 5 5 . . . . . 
+. . . . . . . . . . . . . . . . 
+`];
+            case "myAnim":
+            case "anim2":return [img`
+.................ccfff..............
+................cddbbf..............
+...............cddbbf...............
+..............fccbbcf............ccc
+........ffffffccccccff.........ccbbc
+......ffbbbbbbbbbbbbbcfff.....cdbbc.
+....ffbbbbbbbbbcbcbbbbcccff..cddbbf.
+....fbcbbbbbffbbcbcbbbcccccfffdbbf..
+....fbbb1111ff1bcbcbbbcccccccbbbcf..
+.....fb11111111bbbbbbcccccccccbccf..
+......fccc33cc11bbbbccccccccfffbbcf.
+.......fc131c111bbbcccccbdbc...fbbf.
+........f33c111cbbbfdddddcc.....fbbf
+.........ff1111fbdbbfddcc........fff
+...........cccccfbdbbfc.............
+.................fffff..............
+`, img`
+.................ccfff..............
+................cddbbf..............
+...............cddbbf...............
+.........ffffffccbbcf...............
+......fffbbbbbbbbcccff..............
+.....fbbbbbbbbbbbbbbbcfff......ccccc
+.....bcbbbbbffbcbcbbbbcccff...cdbbbc
+.....bbb1111ffbbcbcbbbcccccffcddbbc.
+.....fb11111111bcbcbbbcccccccbdbbf..
+......fccc33c11bbbbbbcccccccccbbcf..
+.......fc131cc11bbbbccccccccffbccf..
+........f33c1111bbbcccccbdbc..fbbcf.
+.........ff1111cbbbfdddddcc....fbbf.
+...........ccc1fbdbbfddcc.......fbbf
+..............ccfbdbbfc..........fff
+.................fffff..............
+`, img`
+..................ccfff.............
+.................cddbbf.............
+........fffffffffddbbf..............
+.......fbbbbbbbbbfcbcf..............
+.......fbbc111bffbbccffff...........
+.......fb111111ffbbbbbcccff....ccccc
+........f1cc3311bbcbcbbccccf..cdbbbc
+........fcc131c1bbbcbcbcccccfcddbbc.
+.........f111111bbbcbcbccccccbdbbf..
+.........f1111111bbbbbccccccccbbcf..
+..........f111111bbbbcccccccffbccf..
+...........c1111cbbbcccccbdbc.fbbcf.
+............cc11cbbbfddddddc...fbbf.
+..............cffbdbbfdddcc.....fbbf
+.................fbdbbfcc........fff
+..................fffff.............
+`, img`
+....................ccfff...........
+..........fffffffffcbbbbf...........
+.........fbbbbbbbbbfffbf............
+.........fbb111bffbbbbff............
+.........fb11111ffbbbbbcff..........
+.........f1cccc11bbcbcbcccf.........
+..........fc1c1c1bbbcbcbcccf...ccccc
+............c3331bbbcbcbccccfccddbbc
+...........c333c1bbbbbbbcccccbddbcc.
+...........c331c11bbbbbcccccccbbcc..
+..........cc13c111bbbbccccccffbccf..
+..........c111111cbbbcccccbbc.fccf..
+...........cc1111cbbbfdddddc..fbbcf.
+.............cccffbdbbfdddc....fbbf.
+..................fbdbbfcc......fbbf
+...................fffff.........fff
+`, img`
+...........fffffff...ccfff..........
+..........fbbbbbbbffcbbbbf..........
+..........fbb111bbbbbffbf...........
+..........fb11111ffbbbbff...........
+..........f1cccc1ffbbbbbcff.........
+..........ffc1c1c1bbcbcbcccf........
+...........fcc3331bbbcbcbcccf..ccccc
+............c333c1bbbcbcbccccfcddbbc
+............c333c1bbbbbbbcccccddbcc.
+............c333c11bbbbbccccccbbcc..
+...........cc331c11bbbbccccccfbccf..
+...........cc13c11cbbbcccccbbcfccf..
+...........c111111cbbbfdddddc.fbbcf.
+............cc1111fbdbbfdddc...fbbf.
+..............cccfffbdbbfcc.....fbbf
+....................fffff........fff
+`, img`
+....................................
+....................................
+....................................
+...............ccffff...............
+..............cddbbbf...............
+.......ffffffcddbbbf................
+.....ffbbbbbbbbbbbbbcfff.......ccccc
+...ffbbbbbbbbcbcbbbbbcccff....cdbbbc
+..fbbbbbbbbbbcbbcbbbbcccccfffcddbbc.
+.fbcbbbbbbbbbbcbcbbbbccccccccbdbbf..
+.fbbbbbbbfffbbcbbbbbccccccccccbbcf..
+.ffbb1111fffbbcbbbbcccccccbcffbccf..
+..ff111111111bbbbccccccbbbcc..fbbcf.
+....ccccccc111bdbbbfddbccc.....ffbbf
+........ccccccfbdbbbfcc..........fff
+...............ffffff...............
+`];
+        }
+        return null;
+    })
+
+    helpers._registerFactory("song", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+    helpers._registerFactory("json", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
